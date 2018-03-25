10687bbd8bd0638580a1a302d3f3031759d0b422
Igor85342 <imoskalets@gmail.com>
2018-03-25 12:59:33 +0300

    Two files.

---
+++ src/ru/moskalets/one/HelloWorld1.java
@@ -0,0 +1,10 @@
+package ru.moskalets.one;
+
+/**
+ * Created by Пользователь on 25.03.2018.
+ */
+public class HelloWorld1 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
---
+++ src/ru/moskalets/one/HelloWorld2.java
@@ -0,0 +1,10 @@
+package ru.moskalets.one;
+
+/**
+ * Created by Пользователь on 25.03.2018.
+ */
+public class HelloWorld2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World 2");
+    }
+}
